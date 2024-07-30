781a0192d59d394c34c2fe756dfb0153203da0a0
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-07-30 21:10:03 +0100

    set interval to display next image every 5sec

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const previous = document.querySelector(".btn-previous");
 
 display();
 
+setInterval(displayNext, 5000);
+
 const btnSelect = document.querySelectorAll(".btn-select");
 btnSelect.forEach((select) => {
   select.addEventListener("click", () => {
@@ -21,17 +23,7 @@ btnSelect.forEach((select) => {
   });
 });
 
-next.addEventListener("click", () => {
-  const active = document.querySelector(".active");
-  const activeSlideNumber = Number(active.id.slice(-1));
-  const nextSlideNumber =
-    activeSlideNumber + 1 > numberOfSlides ? 1 : activeSlideNumber + 1;
-  const nextSlide = document.getElementById("slide-" + nextSlideNumber);
-  //   console.log(nextSlide);
-  nextSlide.classList.toggle("active");
-  active.classList.toggle("active");
-  display();
-});
+next.addEventListener("click", () => displayNext());
 
 previous.addEventListener("click", () => {
   const active = document.querySelector(".active");
@@ -48,8 +40,26 @@ function display() {
   slides.forEach((slide) => {
     if (!slide.classList.contains("active")) {
       slide.style.display = "none";
+      const nonActiveSlideNumber = Number(slide.id.slice(-1));
+      const selectBtn = document.getElementById("s-" + nonActiveSlideNumber);
+      selectBtn.style.backgroundColor = "#fff";
     } else {
       slide.style.display = displayType;
+      const activeSlideNumber = Number(slide.id.slice(-1));
+      const selectedBtn = document.getElementById("s-" + activeSlideNumber);
+      selectedBtn.style.backgroundColor = "#000";
     }
   });
+}
+
+function displayNext() {
+  const active = document.querySelector(".active");
+  const activeSlideNumber = Number(active.id.slice(-1));
+  const nextSlideNumber =
+    activeSlideNumber + 1 > numberOfSlides ? 1 : activeSlideNumber + 1;
+  const nextSlide = document.getElementById("slide-" + nextSlideNumber);
+  //   console.log(nextSlide);
+  nextSlide.classList.toggle("active");
+  active.classList.toggle("active");
+  display();
 }
